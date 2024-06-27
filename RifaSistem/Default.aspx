efb3729328e1760ebb40a68babe5128ea91a6ed7
Eze123 <146776125+Eze321@users.noreply.github.com>
2024-06-27 15:02:47 -0300

    PruebaSubir
    Subir

---
+++ RifaSistem/Default.aspx
@@ -0,0 +1,82 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="RifaSistem.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Registro de Participantes para la Rifa</title>
+    <link rel="stylesheet" href="styles.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+            <h2>Registro de Participantes para la Rifa</h2>
+            
+            <div>
+                <asp:Label ID="lblNombre" runat="server" Text="Nombre:"></asp:Label>
+                <br />
+                <asp:TextBox ID="txtNombre" runat="server"></asp:TextBox>
+            </div>
+            <div>
+                <asp:Label ID="lblApellido" runat="server" Text="Apellido:"></asp:Label>
+                <br />
+                <asp:TextBox ID="txtApellido" runat="server"></asp:TextBox>
+            </div>
+            <div>
+                <asp:Label ID="lblNumeroElegido" runat="server" Text="Número Elegido:"></asp:Label>
+                <br />
+                <asp:TextBox ID="txtNumeroElegido" runat="server"></asp:TextBox>
+            </div>
+
+            <div class="containerR">
+                
+                <div class="radio-group">
+                    <asp:Label ID="lblPago" runat="server" Text="¿Pagó?:"></asp:Label>
+                <asp:RadioButtonList ID="rblPago" runat="server">
+                    <asp:ListItem Text="Sí" Value="true"></asp:ListItem>
+                    <asp:ListItem Text="No" Value="false"></asp:ListItem>
+                </asp:RadioButtonList>
+                    </div>
+            </div>
+            <div>
+                <asp:Button ID="btnInsertar" runat="server" Text="Insertar" CssClass="btn" OnClick="btnInsertar_Click" />
+                <asp:Button ID="btnVaciarBaseDatos" runat="server" Text="Vaciar Base de Datos" CssClass="btn" OnClick="btnVaciarBaseDatos_Click" />
+            </div>
+            <div>
+                <asp:Label ID="lblMessage" runat="server" Text="" ForeColor="Red"></asp:Label>
+            </div>
+            
+            <h2>Participantes Registrados</h2>
+            <div class="grid-container">
+                <asp:GridView ID="GridViewParticipantes" runat="server" AutoGenerateColumns="False" DataKeyNames="NumeroElegido"
+                    OnRowEditing="GridViewParticipantes_RowEditing" OnRowCancelingEdit="GridViewParticipantes_RowCancelingEdit"
+                    OnRowUpdating="GridViewParticipantes_RowUpdating" OnRowCommand="GridViewParticipantes_RowCommand">
+                    <Columns>
+                        <asp:BoundField DataField="Nombre" HeaderText="Nombre" ReadOnly="true" />
+                        <asp:BoundField DataField="Apellido" HeaderText="Apellido" ReadOnly="true" />
+                        <asp:BoundField DataField="NumeroElegido" HeaderText="Número Elegido" ReadOnly="true" />
+                        <asp:TemplateField HeaderText="¿Pagó?">
+                            <ItemTemplate>
+                                <%# (bool)Eval("Pago") ? "Sí" : "No" %>
+                            </ItemTemplate>
+                            <EditItemTemplate>
+                                <asp:RadioButtonList ID="rblEditPago" runat="server">
+                                    <asp:ListItem Text="Sí" Value="true"></asp:ListItem>
+                                    <asp:ListItem Text="No" Value="false"></asp:ListItem>
+                                </asp:RadioButtonList>
+                            </EditItemTemplate>
+                        </asp:TemplateField>
+                        <asp:CommandField ShowEditButton="True" />
+                        <asp:TemplateField HeaderText="Acciones">
+                        <ItemTemplate>
+                            <asp:LinkButton CssClass="btnEliminar" runat="server" CommandName="Eliminar" Text="Eliminar" CommandArgument='<%# Container.DataItemIndex %>' OnClientClick="return confirm('¿Estás seguro que deseas eliminar este participante?');" />
+                        </ItemTemplate>
+                        </asp:TemplateField>
+
+                    </Columns>
+                </asp:GridView>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ RifaSistem/Default.aspx.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace RifaSistem
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CargarParticipantes();
+            }
+        }
+
+        protected void btnInsertar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            int numeroElegido;
+
+            if (!int.TryParse(txtNumeroElegido.Text, out numeroElegido))
+            {
+                lblMessage.Text = "Número elegido debe ser un número entero.";
+                return;
+            }
+
+            bool pago = rblPago.SelectedValue == "true";
+
+            string connectionString = ConfigurationManager.ConnectionStrings["RifaDBConnectionString"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                string query = "INSERT INTO Participantes (Nombre, Apellido, NumeroElegido, Pago) VALUES (@Nombre, @Apellido, @NumeroElegido, @Pago)";
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                cmd.Parameters.AddWithValue("@Nombre", nombre);
+                cmd.Parameters.AddWithValue("@Apellido", apellido);
+                cmd.Parameters.AddWithValue("@NumeroElegido", numeroElegido);
+                cmd.Parameters.AddWithValue("@Pago", pago);
+
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    lblMessage.Text = "Participante registrado correctamente.";
+                }
+                catch (SqlException ex)
+                {
+                    if (ex.Number == 2627) // Error de duplicación
+                    {
+                        lblMessage.Text = "El número elegido ya está registrado. Por favor, elija otro número.";
+                    }
+                    else
+                    {
+                        lblMessage.Text = $"Error de base de datos: {ex.Message}";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    lblMessage.Text = $"Error: {ex.Message}";
+                }
+
+                CargarParticipantes();
+            }
+        }
+
+        private void CargarParticipantes()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["RifaDBConnectionString"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                string query = "SELECT Nombre, Apellido, NumeroElegido, Pago FROM Participantes ORDER BY NumeroElegido ASC";
+                SqlDataAdapter da = new SqlDataAdapter(query, con);
+                DataTable dt = new DataTable();
+
+                try
+                {
+                    da.Fill(dt);
+                }
+                catch (Exception ex)
+                {
+                    lblMessage.Text = $"Error al cargar los participantes: {ex.Message}";
+                }
+
+                GridViewParticipantes.DataSource = dt;
+                GridViewParticipantes.DataBind();
+            }
+        }
+
+        protected void GridViewParticipantes_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridViewParticipantes.EditIndex = e.NewEditIndex;
+            CargarParticipantes();
+        }
+
+        protected void GridViewParticipantes_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridViewParticipantes.EditIndex = -1;
+            CargarParticipantes();
+        }
+
+        protected void GridViewParticipantes_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            int numeroElegido = (int)GridViewParticipantes.DataKeys[e.RowIndex].Value;
+            RadioButtonList rblPago = (RadioButtonList)GridViewParticipantes.Rows[e.RowIndex].FindControl("rblEditPago");
+            bool pago = rblPago.SelectedValue == "true";
+
+            string connectionString = ConfigurationManager.ConnectionStrings["RifaDBConnectionString"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                string query = "UPDATE Participantes SET Pago = @Pago WHERE NumeroElegido = @NumeroElegido";
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                cmd.Parameters.AddWithValue("@Pago", pago);
+                cmd.Parameters.AddWithValue("@NumeroElegido", numeroElegido);
+
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    lblMessage.Text = "Participante actualizado correctamente.";
+                }
+                catch (Exception ex)
+                {
+                    lblMessage.Text = $"Error al actualizar: {ex.Message}";
+                }
+
+                GridViewParticipantes.EditIndex = -1;
+                CargarParticipantes();
+            }
+        }
+
+        protected void GridViewParticipantes_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "Eliminar")
+            {
+                int rowIndex = Convert.ToInt32(e.CommandArgument);
+                int numeroElegido = (int)GridViewParticipantes.DataKeys[rowIndex].Value;
+
+                string connectionString = ConfigurationManager.ConnectionStrings["RifaDBConnectionString"].ConnectionString;
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    string query = "DELETE FROM Participantes WHERE NumeroElegido = @NumeroElegido";
+                    SqlCommand cmd = new SqlCommand(query, con);
+
+                    cmd.Parameters.AddWithValue("@NumeroElegido", numeroElegido);
+
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                        lblMessage.Text = "Participante eliminado correctamente.";
+                    }
+                    catch (Exception ex)
+                    {
+                        lblMessage.Text = $"Error al eliminar participante: {ex.Message}";
+                    }
+
+                    CargarParticipantes();
+                }
+            }
+        }
+
+
+        protected void btnVaciarBaseDatos_Click(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["RifaDBConnectionString"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                string query = "DELETE FROM Participantes";
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    lblMessage.Text = "Base de datos vaciada correctamente.";
+                    CargarParticipantes();
+                }
+                catch (Exception ex)
+                {
+                    lblMessage.Text = $"Error al vaciar la base de datos: {ex.Message}";
+                }
+            }
+        }
+    }
+}
